d876da412c84c4fd70da8d47ac7e8a7dc4cc00c7
OuyangLingxin <ouyangmayi@gmail.com>
2023-05-18 20:23:42 -0400

    Error handling ability added

--- calculator.py
+++ calculator.py
@@ -5,24 +5,34 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
-while True:
+with open("output.txt", "a") as f:
+  while True:
     user_input = input("> ")
     tokens = user_input.split(' ')
     if tokens[0] == 'q':
-       break
+      break
+    elif len(tokens) < 3:
+      print("please enter correct number of arguments")
+      break
+    elif tokens[0] not in ['+', '-', '*', '/', 'square', 'cube', 'pow', 'mod']:
+      print("please enter the correct command")
+      break
+    elif type(tokens[1]) != "numbe" or type(tokens[2]) != "numbe":
+      print("please enter numbers only")
+      break
     elif tokens[0] == "+":
-      print(add(float(tokens[1]), float(tokens[2])))
+      print(add(float(tokens[1]), float(tokens[2])), file=f)
     elif tokens[0] == "-":
-      print(subtract(float(tokens[1]), float(tokens[2])))
+      print(subtract(float(tokens[1]), float(tokens[2])), file=f)
     elif tokens[0] == "*":
-      print(multiply(float(tokens[1]), float(tokens[2])))
+      print(multiply(float(tokens[1]), float(tokens[2])), file=f)
     elif tokens[0] == "/":
-      print(divide(float(tokens[1]), float(tokens[2])))
+      print(divide(float(tokens[1]), float(tokens[2])), file=f)
     elif tokens[0] == "square":
-      print(square(float(tokens[1])))
+      print(square(float(tokens[1])), file=f)
     elif tokens[0] == "cube":
-      print(cube(float(tokens[1])))
+      print(cube(float(tokens[1])), file=f)
     elif tokens[0] == "pow":
-      print(power(float(tokens[1]), float(tokens[2])))
+      print(power(float(tokens[1]), float(tokens[2])), file=f)
     elif tokens[0] == "mod":
-      print(mod(float(tokens[1]), float(tokens[2])))
+      print(mod(float(tokens[1]), float(tokens[2])), file=f)
